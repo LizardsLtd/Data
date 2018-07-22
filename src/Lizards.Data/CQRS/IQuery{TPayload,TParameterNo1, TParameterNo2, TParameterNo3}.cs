728be13b4f5952abfc66362c63db1d0e0d08a25f
Szymon Sasin <szymon@sasin.eu>
2018-07-22 15:31:01 +0100

    adde dmore varaition of parametriesd query

---
+++ src/Lizards.Data/CQRS/IQuery{TPayload,TParameterNo1, TParameterNo2, TParameterNo3}.cs
@@ -0,0 +1,7 @@
+﻿namespace Lizards.Data.CQRS
+{
+    public interface IQuery<out TPayload, TParameterNo1, TParameterNo2, TParameterNo3> : IsQuery
+    {
+        TPayload Execute(TParameterNo1 param1, TParameterNo2 param2, TParameterNo3 param3);
+    }
+}
---
+++ src/Lizards.Data/CQRS/IQuery{TPayload,TParameterNo1, TParameterNo2}.cs
@@ -0,0 +1,7 @@
+﻿namespace Lizards.Data.CQRS
+{
+    public interface IQuery<out TPayload, TParameterNo1, TParameterNo2> : IsQuery
+    {
+        TPayload Execute(TParameterNo1 param1, TParameterNo2 param2);
+    }
+}
