091073eb32464a6301ce5643d9cb303f2d9ee8f9
Szymon M Sasin <szymon.sasin@vigilantapps.com>
2018-07-25 16:38:55 +0100

    improving dc wrapper

--- src/Lizzards.Data.Cache/DistributedCache.fs
+++ src/Lizzards.Data.Cache/DistributedCache.fs
@@ -5,21 +5,27 @@ open System
 open Newtonsoft.Json
 
 type DistributedCache(cache: IDistributedCache) =
-  member this.cache = cache
-
-  member this.Serialize key item=
+  member this.Set key item=
     let itemAsJson= JsonConvert.SerializeObject(item)
-    this.cache.SetString(key, itemAsJson)
+    cache.SetString(key, itemAsJson)
 
-  member this.Deserialize<'T> (key: string)  (fallback: unit -> 'T) =
+  member this.ContainsKey key =
+    let item = cache.GetString(key)
+    let result =
+      match item with
+      | null | "" ->false
+      | _ -> true
+    result
+
+  member this.Get<'T> (key: string)  (fallback: unit -> 'T) =
     let (|?) = defaultArg
-    let serilaliziedItem = this.cache.GetString(key)
+    let serilaliziedItem = cache.GetString(key)
     let item =
       match serilaliziedItem with
       | x when System.String.IsNullOrWhiteSpace(x) ->
         let result = fallback()
         let serialized=  JsonConvert.SerializeObject(result)
-        this.cache.SetString(key, serialized)
+        cache.SetString(key, serialized)
         result
       | _ ->
         let result =JsonConvert.DeserializeObject(serilaliziedItem)
